f59ea849fd386d33b47d3aeee0083d47229aa34d
mkarora <mk.arora819@gmail.com>
2025-12-24 15:46:32 -0800

    creating pages

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive],
   templateUrl: "./app.component.html",
-  styleUrls: ["./app.component.scss"],
+  styleUrls: ["./app.component.scss"]
 })
 export class AppComponent {
   title = 'wastewise-frontend';
---
+++ frontend/src/app/monthly-insights/monthly-insights.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'monthly-insights',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: "./monthly-insights.component.html",
+  styleUrls: ["./monthly-insights.component.scss"]
+})
+export class MonthlyInsightsComponent {
+}
+
